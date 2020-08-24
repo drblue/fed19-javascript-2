5a039d99fe8df7ce21a6ea636b4b7442bf7788d3
Johan Nordström <johan@digitalvillage.se>
2020-08-24 13:51:45 +0200

    Updating our items state with the result of the side-effect request.

--- 01-hooks/src/components/ResourceDemo.js
+++ 01-hooks/src/components/ResourceDemo.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 const ResourceDemo = () => {
 	const [resource, setResource] = useState('todos');
+	const [items, setItems] = useState([]);
 
 	// watch `resource` for changes and trigger side-effect
 	useEffect(() => {
@@ -11,6 +12,7 @@ const ResourceDemo = () => {
 		axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
 		.then(res => {
 			console.log("Request returned a result:", res.data);
+			setItems(res.data);
 		});
 	}, [resource]);
 
@@ -19,13 +21,17 @@ const ResourceDemo = () => {
 			<h2>ResourceDemo</h2>
 
 			<div className="btn-group">
+				<button onClick={() => setResource('photos')} className="btn btn-primary">Photos</button>
 				<button onClick={() => setResource('posts')} className="btn btn-primary">Posts</button>
 				<button onClick={() => setResource('todos')} className="btn btn-primary">Todos</button>
-				<button onClick={() => setResource('users')} className="btn btn-primary">Users</button>
 			</div>
 
-			<div className="mt-3">
-				<h3>Showing {resource} items</h3>
+			<div className="mt-3 text-left">
+				<h3>Showing {items.length} {resource}</h3>
+
+				<ul>
+					{items.map((item, index) => (<li key={index}>{item.title}</li>))}
+				</ul>
 			</div>
 		</div>
 	);
