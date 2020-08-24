7415c634cedaca7242caf07737532353b19e9c10
Johan Nordström <johan@digitalvillage.se>
2020-08-24 13:58:22 +0200

    Trigger side-effect when one of the dependencies are changed.

--- 01-hooks/src/components/ResourceDemo.js
+++ 01-hooks/src/components/ResourceDemo.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 const ResourceDemo = () => {
 	const [resource, setResource] = useState('todos');
 	const [items, setItems] = useState([]);
+	const [page, setPage] = useState(1);
 
 	// watch `resource` for changes and trigger side-effect
 	useEffect(() => {
@@ -14,7 +15,7 @@ const ResourceDemo = () => {
 			console.log("Request returned a result:", res.data);
 			setItems(res.data);
 		});
-	}, [resource]);
+	}, [resource, page]);
 
 	return (
 		<div>
@@ -27,7 +28,12 @@ const ResourceDemo = () => {
 			</div>
 
 			<div className="mt-3 text-left">
-				<h3>Showing {items.length} {resource}</h3>
+				<h3>Showing {items.length} {resource} at page {page}</h3>
+
+				<div className="d-flex justify-content-between">
+					<button onClick={() => page > 1 ? setPage(page - 1) : ''} className="btn btn-info">Previous Page</button>
+					<button onClick={() => setPage(page + 1)} className="btn btn-info">Next Page</button>
+				</div>
 
 				<ul>
 					{items.map((item, index) => (<li key={index}>{item.title}</li>))}
