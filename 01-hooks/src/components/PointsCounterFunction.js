ad03ef6e077ee9280b21993045ac3917e098a840
Johan Nordström <johan@digitalvillage.se>
2020-08-24 11:24:49 +0200

    A better way to set state in function-components.

--- 01-hooks/src/components/PointsCounterFunction.js
+++ 01-hooks/src/components/PointsCounterFunction.js
@@ -2,34 +2,38 @@ import React, { useState } from 'react';
 
 function PointsCounterFunction() {
 
-	const [state, setState] = useState({ pointsCount: 1337 });
+	const [points, setPoints] = useState(0);
+	const [supporters, setSupporters] = useState(10);
 
 	const decreasePoints = () => {
-		setState(prevState => ({
-			pointsCount: prevState.pointsCount - 1,
-		}))
-		setState(prevState => ({
-			pointsCount: prevState.pointsCount - 1,
-		}))
+		setPoints(prevPoints => prevPoints - 1);
 	}
 
 	const increasePoints = () => {
-		setState(prevState => ({
-			pointsCount: prevState.pointsCount + 1,
-		}))
-		setState(prevState => ({
-			pointsCount: prevState.pointsCount + 1,
-		}))
+		setPoints(prevPoints => prevPoints + 1);
 	}
 
 	return (
 		<div className="points-counter">
 			<div className="text-center">
-				<p className="display-2">{state.pointsCount}</p>
+				<p className="display-2">{points}</p>
 			</div>
 			<div className="point-buttons btn-group">
 				<button onClick={decreasePoints} className="btn btn-lg btn-danger">-</button>
 				<button onClick={increasePoints} className="btn btn-lg btn-success">+</button>
+			</div>
+
+			<div className="text-center">
+				<p className="display-3">Supporters: {supporters}</p>
+			</div>
+			<div className="point-buttons btn-group">
+				<button onClick={() => {
+					setSupporters(prevSupporters => prevSupporters - 1)
+				}} className="btn btn-lg btn-danger">-</button>
+
+				<button onClick={() => {
+					setSupporters(prevSupporters => prevSupporters + 1)
+				}} className="btn btn-lg btn-success">+</button>
 			</div>
 		</div>
 	);
