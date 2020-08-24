380d0dd961107581b1bb64f5d52076ab5fce7bfe
Johan Nordström <johan@digitalvillage.se>
2020-08-24 10:24:48 +0200

    Simple class-based component (PointsCounter).

--- 01-hooks/src/App.js
+++ 01-hooks/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './App.scss';
+import PointsCounter from './components/PointsCounter';
 
 function App() {
 	return (
 		<div className="App container my-3">
 			<h1><span role="img" aria-label="A fish on a hook">🎣</span> React Hooks</h1>
+
+			<div className="text-center my-5">
+				<h2>Home Team</h2>
+				<PointsCounter />
+
+				<hr className="my-5" />
+
+				<h2>Away Team</h2>
+				<PointsCounter />
+			</div>
 		</div>
 	);
 }
---
+++ 01-hooks/src/components/PointsCounter.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class PointsCounter extends Component {
+	state = {
+		pointsCount: 0,
+	}
+
+	decreasePoints = () => {
+		this.setState({
+			pointsCount: this.state.pointsCount - 1,
+		});
+	}
+
+	increasePoints = () => {
+		this.setState({
+			pointsCount: this.state.pointsCount + 1,
+		});
+	}
+
+	render() {
+		return (
+			<div className="points-counter">
+				<div className="text-center">
+					<p className="display-2">{this.state.pointsCount}</p>
+				</div>
+				<div className="point-buttons btn-group">
+					<button onClick={this.decreasePoints} className="btn btn-lg btn-danger">-</button>
+					<button onClick={this.increasePoints} className="btn btn-lg btn-success">+</button>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default PointsCounter;
