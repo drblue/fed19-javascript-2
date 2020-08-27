e76006e1e96d012c296e467df3e0868adcb8383b
Johan Nordström <johan@digitalvillage.se>
2020-08-27 13:55:20 +0200

    Search Starship, utilizing useState, useEffect and useRef hooks.

--- 01-hooks/src/components/UseRefDomDemo.js
+++ 01-hooks/src/components/UseRefDomDemo.js
@@ -1,31 +1,71 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const STARSHIPS = ["CR90 corvette","Star Destroyer","Sentinel-class landing craft","Death Star","Millennium Falcon","Y-wing","X-wing","TIE Advanced x1","Executor","Rebel transport","Slave 1","Imperial shuttle","EF76 Nebulon-B escort frigate","Calamari Cruiser","A-wing","B-wing","Republic Cruiser","Droid control ship","Naboo fighter","Naboo Royal Starship","Scimitar","J-type diplomatic barge","AA-9 Coruscant freighter","Jedi starfighter","H-type Nubian yacht","Republic Assault ship","Solar Sailer","Trade Federation cruiser","Theta-class T-2c shuttle","Republic attack cruiser","Naboo star skiff","Jedi Interceptor","arc-170","Banking clan frigte","Belbullab-22 starfighter","V-wing"];
 
 const UseRefDomDemo = () => {
+	const [query, setQuery] = useState('');
+	const [searchResult, setSearchResult] = useState([]);
+	const queryRef = useRef();
+
+	const clearSearch = () => {
+		setSearchResult([]);
+		setQuery('');
+		queryRef.current.focus();
+	}
+
+	const searchStarShip = e => {
+		e.preventDefault();
+	}
+
+	useEffect(() => {
+		if (query.length < 3) {
+			return;
+		}
+
+		setSearchResult(
+			STARSHIPS.filter(starship =>
+				starship.toLowerCase().includes(query.toLowerCase())
+			)
+		);
+	}, [query]);
+
+	useEffect(() => {
+		queryRef.current.focus();
+	}, []);
+
 	return (
 		<>
 			<h2 className="text-center mb-2">Search Starship</h2>
 
-			<form>
+			<form onSubmit={searchStarShip}>
 				<div className="form-group">
-					<input type="text" className="form-control" placeholder="Type to search" />
+					<input
+						onChange={e => setQuery(e.target.value)}
+						ref={queryRef}
+						value={query}
+						type="text"
+						className="form-control"
+						placeholder="Type to search" />
 				</div>
 
 				<div className="text-center">
-					<button className="btn btn-primary">Focus</button>
-					<button className="btn btn-success" type="submit">Search</button>
+					<button className="btn btn-primary" onClick={clearSearch}>Clear</button>
 				</div>
 			</form>
 
 			<div className="mt-3">
-				<p>Search for QWERTY resulted in the following matches:</p>
+				{
+					query.length > 2 ? (
+						<>
+							<p>Search for <strong>{query}</strong> resulted in <strong>{searchResult.length}</strong> matches.</p>
 
-				<ul className="search-results">
-					<li>Result 1</li>
-					<li>Result 2</li>
-					<li>Result 3</li>
-				</ul>
+							<ul className="search-results">
+								{searchResult.map((result, key) => (<li key={key}>{result}</li>))}
+							</ul>
+						</>
+					) : ''
+				}
+
 			</div>
 		</>
 	);
