26a22be935c197c43edcfbfbc4cb6328c7ca9802
Johan Nordström <johan@digitalvillage.se>
2020-08-27 10:56:27 +0200

    THIS JUST IN! YOU CAN CHANGE RESOURCE!! OMG!

--- 01-hooks/src/components/StarWarsPedia.js
+++ 01-hooks/src/components/StarWarsPedia.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+const API_BASE = 'https://swapi.dev/api';
+
 const StarWarsPedia = () => {
-	const [resourceUrl, setResourceUrl] = useState('https://swapi.dev/api/vehicles/');
+
+	const [resourceUrl, setResourceUrl] = useState(API_BASE + '/vehicles/');
 	const [response, setResponse] = useState({});
 
 	// watch `resourceUrl` for changes and trigger fetching of data from the new `resourceUrl`
@@ -22,7 +25,11 @@ const StarWarsPedia = () => {
 			<h2>StarWarsPedia</h2>
 
 			<div className="resource-buttons">
-				<button onClick={() => {}} className="btn btn-outline-primary">Vehicles</button>
+				<button onClick={() => setResourceUrl(API_BASE + '/people/')} className="btn btn-outline-primary"><span role="img" aria-label="People">🧑‍🤝‍🧑</span> People</button>
+				<button onClick={() => setResourceUrl(API_BASE + '/planets/')} className="btn btn-outline-primary"><span role="img" aria-label="Planet">🪐</span> Planets</button>
+				<button onClick={() => setResourceUrl(API_BASE + '/species/')} className="btn btn-outline-primary"><span role="img" aria-label="DNA">🧬</span> Species</button>
+				<button onClick={() => setResourceUrl(API_BASE + '/starships/')} className="btn btn-outline-primary"><span role="img" aria-label="UFO">🛸</span> Starships</button>
+				<button onClick={() => setResourceUrl(API_BASE + '/vehicles/')} className="btn btn-outline-primary"><span role="img" aria-label="Car">🚙</span> Vehicles</button>
 			</div>
 
 			<div className="resource-result text-left mt-3">
