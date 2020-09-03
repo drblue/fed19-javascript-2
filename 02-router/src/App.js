47bf127248b5ac6398a2bd025142ebc5edec628b
Johan Nordström <johan@digitalvillage.se>
2020-09-03 10:38:53 +0200

    Removed some console.log's that was annoying.

--- 02-router/src/App.js
+++ 02-router/src/App.js
@@ -1,21 +1,11 @@
-import React, { useEffect } from 'react';
-import { Switch, Route, useLocation } from 'react-router-dom';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.scss';
 import HackerNewsArticle from './components/HackerNewsArticle';
 import HackerNewsSearch from './components/HackerNewsSearch';
 import HackerNewsLatest from './components/HackerNewsLatest';
 
 function App() {
-	const location = useLocation();
-	useEffect(() => {
-		console.log("location.pathname changed!", location.pathname);
-	}, [location.pathname]);
-
-	const user = {
-		email: 'jn@thehiveresistance.com',
-		loggedInSince: 1598948811
-	}
-
 	return (
 		<div className="App container my-3">
 			<h1><span role="img" aria-label="A gear">⚙️</span> React Router (with Hooks!)</h1>
@@ -31,7 +21,7 @@ function App() {
 					</Route>
 
 					<Route path='/articles/:articleId'>
-						<HackerNewsArticle user={user} />
+						<HackerNewsArticle />
 					</Route>
 				</Switch>
 			</div>
--- 02-router/src/components/HackerNewsSearch.js
+++ 02-router/src/components/HackerNewsSearch.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import useFetch from '../hooks/useFetch';
 
 const HackerNewsSearch = () => {
@@ -7,8 +7,6 @@ const HackerNewsSearch = () => {
 	const [{ data, isLoading, error }, setUrl] = useFetch('');
 	const queryRef = useRef();
 	const searchQuery = useRef('');
-	const location = useLocation();
-	console.log('location:', location);
 
 	useEffect(() => {
 		queryRef.current.focus();
