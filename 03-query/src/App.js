a5bf43ee845d1139fa52c1d483a95a1d880d55e6
Johan Nordström <johan@digitalvillage.se>
2020-09-09 15:09:27 +0200

    React Query DevTools to the rescue 🚒🚨!

--- 03-query/src/App.js
+++ 03-query/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { ReactQueryDevtools } from 'react-query-devtools';
 import './App.scss';
 import GitHubProfile from './components/GitHubProfile';
 import GitHubRepositories from './components/GitHubRepositories';
@@ -8,15 +9,18 @@ function App() {
 	const [page, setPage] = useState('profile');
 
 	return (
-		<div className="App container my-3">
-			<h1><span role="img" aria-label="!?">❗️❓</span> React Query</h1>
+		<>
+			<div className="App container my-3">
+				<h1><span role="img" aria-label="!?">❗️❓</span> React Query</h1>
 
-			<Navbar setPage={setPage} />
+				<Navbar setPage={setPage} />
 
-			<div className="my-5">
-				{page === 'profile' ? <GitHubProfile /> : <GitHubRepositories />}
+				<div className="my-5">
+					{page === 'profile' ? <GitHubProfile /> : <GitHubRepositories />}
+				</div>
 			</div>
-		</div>
+			<ReactQueryDevtools initialIsOpen position="bottom-right" />
+		</>
 	);
 }
 
