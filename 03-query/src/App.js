c8f6ed55d3aeb158a272c6295c477ab7378bbba0
Johan Nordström <johan@digitalvillage.se>
2020-09-09 10:00:27 +0200

    Empty boilerplate for React Query demo.

---
+++ 03-query/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+	return (
+		<div className="App container my-3">
+			<h1><span role="img" aria-label="!?">❗️❓</span> React Query</h1>
+
+			<div className="my-5">
+				Here be content!
+			</div>
+		</div>
+	);
+}
+
+export default App;
