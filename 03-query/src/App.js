ae63a2ffbcb6d5f21c4ecbfaa75f5f8a8ed0ef8c
Johan Nordström <johan@digitalvillage.se>
2020-09-09 10:34:27 +0200

    Some placeholder HTML for our GitHub user profile stats.

--- 03-query/src/App.js
+++ 03-query/src/App.js
@@ -7,7 +7,26 @@ function App() {
 			<h1><span role="img" aria-label="!?">❗️❓</span> React Query</h1>
 
 			<div className="my-5">
-				Here be content!
+				<h2>Name</h2>
+				<h3 className="mb-3">Company</h3>
+				<p className="mb-3">Biography</p>
+
+				<ul className="list-group list-group-horizontal">
+					<li className="list-group-item">
+						<span role="img" aria-label="two eyes">👀</span>{' '}
+						<strong>X</strong> following
+					</li>
+
+					<li className="list-group-item">
+						<span role="img" aria-label="three shining stars">✨</span>{' '}
+						<strong>Y</strong> followers
+					</li>
+
+					<li className="list-group-item">
+						<span role="img" aria-label="three books stacked on eachother">📚</span>{' '}
+						<strong>Z</strong> public repositories
+					</li>
+				</ul>
 			</div>
 		</div>
 	);
