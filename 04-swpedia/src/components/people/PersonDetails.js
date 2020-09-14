b20e478cb72c690d97f3da4fc1a6cbb378571114
Johan Nordström <johan@digitalvillage.se>
2020-09-14 11:57:26 +0200

    Show a person's links

--- 04-swpedia/src/components/people/PersonDetails.js
+++ 04-swpedia/src/components/people/PersonDetails.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import { useQuery } from 'react-query';
 import MoonLoader from 'react-spinners/MoonLoader';
 import { getPerson } from '../../services/SWAPI';
+import { getIdFromUrl } from '../../helpers';
 
 const PersonDetails = () => {
 	const navigate = useNavigate();
@@ -24,13 +25,16 @@ const PersonDetails = () => {
 		)
 	}
 
+	const homeworld_id = getIdFromUrl(data.homeworld);
+
 	return (
 		<div className="card">
 			<div className="card-header">
-				<h2 className="card-title h5 mb-0">{data.name}</h2>
+				<h1 className="card-title h5 mb-0">{data.name}</h1>
 			</div>
 
 			<div className="card-body">
+				<h2 className="h4">Attributes</h2>
 				<dl className="row">
 					<dt className="col-sm-3">Gender</dt>
 					<dd className="col-sm-9">{data.gender}</dd>
@@ -43,6 +47,37 @@ const PersonDetails = () => {
 
 					<dt className="col-sm-3">Mass</dt>
 					<dd className="col-sm-9">{data.mass} kg</dd>
+
+					<dt className="col-sm-3">Hair color</dt>
+					<dd className="col-sm-9">{data.hair_color}</dd>
+
+					<dt className="col-sm-3">Skin color</dt>
+					<dd className="col-sm-9">{data.skin_color}</dd>
+
+					<dt className="col-sm-3">Eye color</dt>
+					<dd className="col-sm-9">{data.eye_color}</dd>
+				</dl>
+
+				<h2 className="h4">Links</h2>
+				<dl className="row">
+					<dt className="col-sm-3">Homeworld</dt>
+					<dd className="col-sm-9">
+						<Link to={`/planets/${homeworld_id}`}>Planet {homeworld_id} &raquo;</Link>
+					</dd>
+
+					<dt className="col-sm-3">Films</dt>
+					<dd className="col-sm-9">
+						<ul className="list-group">
+							{data.films.map(url => {
+								const id = getIdFromUrl(url);
+								return (
+									<li className="list-group-item" key={id}>
+										<Link to={`/films/${id}`}>Film {id} &raquo;</Link>
+									</li>
+								)
+							})}
+						</ul>
+					</dd>
 				</dl>
 
 				<button onClick={() => navigate(-1)} className="btn btn-secondary">&laquo; Back</button>
