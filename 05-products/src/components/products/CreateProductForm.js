fb277977442109376fbc0fe97b22924c33faf13a
Johan Nordström <johan@digitalvillage.se>
2020-09-16 12:11:26 +0200

    Form for creating a new product, currently not actually submitting.

---
+++ 05-products/src/components/products/CreateProductForm.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+
+const CreateProductForm = () => {
+	const [product, setProduct] = useState({});
+
+	const handleInputChange = (e) => {
+		setProduct({
+			...product,
+			[e.target.id]: e.target.value
+		});
+	}
+
+	const handleFormSubmit = (e) => {
+		e.preventDefault();
+		console.log("Form submitting...");
+	}
+
+	return (
+		<>
+			<h3>Create new product</h3>
+			<form onSubmit={handleFormSubmit}>
+				<div className="form-group">
+					<label htmlFor="name">Product name</label>
+					<input
+						type="text"
+						id="name"
+						onChange={handleInputChange}
+						className="form-control"
+						placeholder="Enter the product name" />
+				</div>
+
+				<div className="form-group">
+					<label htmlFor="price">Price (incl. currency)</label>
+					<input
+						type="text"
+						id="price"
+						onChange={handleInputChange}
+						className="form-control" placeholder="" />
+				</div>
+
+				<div className="form-group">
+					<label htmlFor="description">Description</label>
+					<textarea
+						id="description"
+						rows="10"
+						onChange={handleInputChange}
+						className="form-control"></textarea>
+				</div>
+
+				<div className="form-group">
+					<label htmlFor="SKU">SKU</label>
+					<input
+						type="text"
+						id="SKU"
+						onChange={handleInputChange}
+						className="form-control"
+						placeholder="" />
+				</div>
+
+				<div className="form-group">
+					<label htmlFor="EAN">EAN</label>
+					<input
+						type="text"
+						id="EAN"
+						onChange={handleInputChange}
+						className="form-control"
+						placeholder="" />
+				</div>
+
+				<div className="d-flex justify-content-end">
+					<button type="submit" className="btn btn-primary">Create</button>
+				</div>
+			</form>
+		</>
+	);
+}
+
+export default CreateProductForm;
--- 05-products/src/components/products/Products.js
+++ 05-products/src/components/products/Products.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from 'react-query';
 import MoonLoader from 'react-spinners/MoonLoader';
 import { getProducts } from '../../services/ShopAPI';
+import CreateProductForm from './CreateProductForm';
 import Product from './Product';
 
 const Products = () => {
 
+	const [showCreateProductForm, setShowCreateProductForm] = useState(false);
 	const { data, isLoading, error } = useQuery(['products'], getProducts);
 
 	if (isLoading) {
@@ -31,11 +33,22 @@ const Products = () => {
 
 			<div className="row">
 				{data.map((product, index) => (
-					<div className="col-lg-3 col-md-4 col-sm-6" key={index}>
+					<div className="col-lg-4 col-md-6" key={index}>
 						<Product product={product} />
 					</div>
 				))}
 			</div>
+
+			{
+				showCreateProductForm
+				? (
+					<CreateProductForm />
+				) : (
+					<div className="d-flex justify-content-end">
+						<button className="btn btn-primary btn-lg" onClick={() => setShowCreateProductForm(true)}>+</button>
+					</div>
+				)
+			}
 		</>
 	);
 }
