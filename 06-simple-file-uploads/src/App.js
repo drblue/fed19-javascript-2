2558f8aa46c0a18c12e6e8fa84953cadca427875
Johan Nordström <johan@digitalvillage.se>
2020-12-01 10:47:10 +0100

    Added Bootstrap and node-sass v4.

--- 06-simple-file-uploads/src/App.js
+++ 06-simple-file-uploads/src/App.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
+import Alert from 'react-bootstrap/Alert';
+import Container from 'react-bootstrap/Container';
+import './App.scss';
 
 function App() {
 	return (
-		<div className="App">
+		<Container>
+			<Alert variant="info" className="my-5">This is a purely informational alert.</Alert>
+
 			<header className="App-header">
 				<h1>Simple File Uploads</h1>
 			</header>
-		</div>
+		</Container>
 	);
 }
 
