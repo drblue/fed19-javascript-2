119b90c830fcae7d8eb0148cb3ab32fa77d43911
Johan Nordström <johan@digitalvillage.se>
2020-12-01 12:14:01 +0100

    Upload image to Firebase Cloud Storage, and then retrieve the URL to the uploaded image.

--- 06-simple-file-uploads/src/App.js
+++ 06-simple-file-uploads/src/App.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { storage } from './firebase';
+import Alert from 'react-bootstrap/Alert';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
@@ -6,18 +8,56 @@ import './App.scss';
 
 function App() {
 	const [image, setImage] = useState(null);
+	const [alertMsg, setAlertMsg] = useState(null);
+	const [uploadedImageUrl, setUploadedImageUrl] = useState(null);
 
 	const handleFileChange = e => {
 		if (e.target.files[0]) {
 			setImage(e.target.files[0]);
 		}
-		console.log("File changed!", e.target.files[0]);
+		console.log("File changed!", e.target.files);
 	}
 
 	const handleSubmit = e => {
 		e.preventDefault();
 
-		console.log("Would upload file to storage");
+		if (!image) {
+			return;
+		}
+
+		// get root reference
+		const storageRef = storage.ref();
+
+		// create a reference based on the file's name
+		const fileRef = storageRef.child(image.name);
+
+		// put (upload) image to fileRef
+		const uploadTask = fileRef.put(image);
+
+		// are we there yet?
+		uploadTask.then(snapshot => {
+			console.log("File has been uploaded!", snapshot);
+
+			// let user know we're done
+			setAlertMsg({
+				type: "success",
+				msg: "Image successfully uploaded",
+			});
+
+			// retrieve URL to uploaded file
+			snapshot.ref.getDownloadURL().then(url => {
+				setUploadedImageUrl(url);
+			});
+
+		}).catch(error => {
+			console.error("File upload triggered an error!", error);
+			setAlertMsg({
+				type: "warning",
+				msg: `Image could not be uploaded due to an error (${error.code})`
+			});
+		});
+
+		console.log("uploadTask:", uploadTask);
 	}
 
 	const handleReset = e => {
@@ -47,6 +87,14 @@ function App() {
 							: "No image selected."
 					}
 				</div>
+
+				{
+					alertMsg && (<Alert variant={alertMsg.type}>{alertMsg.msg}</Alert>)
+				}
+
+				{
+					uploadedImageUrl && (<img src={uploadedImageUrl} className="img-fluid my-3" alt="uploaded file" />)
+				}
 
 				<Button variant="primary" type="submit">Upload</Button>
 				<Button variant="secondary" type="reset">Clear</Button>
