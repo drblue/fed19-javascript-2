d1c3b074506b2363cfda4096bb36bb5acd1bd857
Johan Nordström <johan@digitalvillage.se>
2020-12-03 14:28:02 +0100

    Removed unnecessary dependencies.

--- 06-simple-file-uploads/src/App.js
+++ 06-simple-file-uploads/src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { db, storage } from './firebase';
 import Alert from 'react-bootstrap/Alert';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
