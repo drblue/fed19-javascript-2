a4086b41850d58d2b478d7b15fad9e747f004ed5
Johan Nordström <johan@digitalvillage.se>
2020-12-04 15:09:21 +0100

    Delete a file from storage and db.

--- 06-simple-file-uploads/src/components/CardImage.js
+++ 06-simple-file-uploads/src/components/CardImage.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
+import useDeleteImage from '../hooks/useDeleteImage';
 
 const CardImage = ({ image }) => {
+	const [deleteImage, setDeleteImage] = useState(null);
+	useDeleteImage(deleteImage);
+
+	const handleDeleteImage = () => {
+		// eslint-disable-next-line no-restricted-globals
+		if (confirm(`Are you really sure you want to delete the image\n"${image.name}"?`)) {
+			setDeleteImage(image);
+		}
+	}
+
 	return (
 		<Col sm={6} md={4} lg={3}>
 			<Card className="mb-3">
@@ -10,9 +22,10 @@ const CardImage = ({ image }) => {
 					<Card.Img variant="top" src={image.url} />
 				</a>
 				<Card.Body>
-					<Card.Text>
+					<Card.Text className="text-muted small">
 						{image.name} ({Math.round(image.size/1024)} kb)
 					</Card.Text>
+					<Button variant="danger" size="sm" onClick={handleDeleteImage}>Delete</Button>
 				</Card.Body>
 			</Card>
 		</Col>
