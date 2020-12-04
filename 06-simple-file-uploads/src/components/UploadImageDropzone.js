3d8fae75acf0732550822604f038623779f0dc87
Johan Nordström <johan@digitalvillage.se>
2020-12-04 11:46:01 +0100

    Style dropzone if file being dropped is valid or not.

--- 06-simple-file-uploads/src/components/UploadImageDropzone.js
+++ 06-simple-file-uploads/src/components/UploadImageDropzone.js
@@ -3,19 +3,32 @@ import { useDropzone } from 'react-dropzone';
 import useUploadImage from '../hooks/useUploadImage';
 
 const UploadImageDropzone = () => {
+
 	const onDrop = useCallback(acceptedFiles => {
 		console.log("Got me zum files 🗃🥳:", acceptedFiles);
 	}, []);
-	const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+	const { getRootProps, getInputProps, isDragActive, acceptedFiles, isDragAccept, isDragReject } = useDropzone({
+		accept: 'image/gif, image/jpeg, image/png',
+		onDrop
+	});
 
 	return (
-		<div {...getRootProps()} id="upload-image-dropzone-wrapper" className="text-center px-4 py-3 my-3">
+		<div {...getRootProps()} id="upload-image-dropzone-wrapper" className={`text-center px-4 py-3 my-3 ${isDragAccept ? `drag-accept`: ``} ${isDragReject ? `drag-reject`: ``}`}>
 			<input {...getInputProps()} />
 			{
 				isDragActive
-					? <img src="/assets/images/drop.gif" className="img-fluid" alt="Drop it! 🔫" />
+					? isDragAccept ? <p>Drop it! 🔫</p> : <p>We don't want that file! 😨</p>
 					: <p>Give me some files 😋!</p>
 			}
+			{acceptedFiles && (
+				<div className="accepted-files mt-2">
+					<ul className="list-unstyled">
+						{acceptedFiles.map(file => (
+							<li key={file.name}>{file.name} ({Math.round(file.size / 1024)} kb)</li>
+						))}
+					</ul>
+				</div>
+			)}
 		</div>
 	)
 }
