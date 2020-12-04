901368793025223e8eb98cfc1e4709fe1dd9850d
Johan Nordström <johan@digitalvillage.se>
2020-12-04 10:38:41 +0100

    Moved upload-form to its own component.

--- 06-simple-file-uploads/src/App.js
+++ 06-simple-file-uploads/src/App.js
@@ -1,69 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import Alert from 'react-bootstrap/Alert';
-import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
-import ProgressBar from 'react-bootstrap/ProgressBar';
 import './App.scss';
 import CardImage from './components/CardImage';
+import UploadImage from './components/UploadImage';
 import useImages from './hooks/useImages';
-import useUploadImage from './hooks/useUploadImage';
-
-const allowedFiletypes = ['image/gif', 'image/jpeg', 'image/png'];
-const maxFileSize = 5;
-const maxFileSizeInBytes = maxFileSize * 1024 * 1024;
 
 function App() {
-	const [file, setFile] = useState(null);
-	const [uploadFile, setUploadFile] = useState(null);
-	const [alertMsg, setAlertMsg] = useState(null);
 	const { images } = useImages();
-	const { uploadProgress, error, isSuccess } = useUploadImage(uploadFile);
-
-	useEffect(() => {
-		if (error) {
-			setAlertMsg(error);
-		} else if (isSuccess) {
-			setAlertMsg({
-				type: 'success',
-				msg: 'Image succesfully uploaded.',
-			});
-		}
-	}, [isSuccess, error]);
-
-	const handleFileChange = e => {
-		const selectedFile = e.target.files[0];
-
-		if (selectedFile) {
-			if (allowedFiletypes.includes(selectedFile.type) && selectedFile.size < maxFileSizeInBytes) {
-				setFile(selectedFile);
-				setAlertMsg(null);
-			} else {
-				setAlertMsg({
-					type: 'warning',
-					msg: `Please select a valid file (gif, jpg or png) with a maximum filesize of ${maxFileSize} mb.`,
-				});
-				setFile(null);
-			}
-		}
-	}
-
-	const handleSubmit = e => {
-		e.preventDefault();
-
-		if (!file) {
-			return;
-		}
-
-		setUploadFile(file);
-	}
-
-	const handleReset = e => {
-		setAlertMsg(null);
-		setFile(null);
-		setUploadFile(null);
-	}
 
 	return (
 		<Container>
@@ -79,41 +23,10 @@ function App() {
 				}
 			</Row>
 
-			<Form onSubmit={handleSubmit} onReset={handleReset}>
-				<h2>Upload a new image</h2>
+			<hr />
 
-				<Form.Group>
-					<Form.File
-						id="upload-image"
-						label="Choose image to upload"
-						custom
-						onChange={handleFileChange}
-					/>
-				</Form.Group>
+			<UploadImage />
 
-				<div className="mb-2">
-					{
-						file
-							? `${file.name} (${Math.round(file.size/1024)} kb)`
-							: "No image selected."
-					}
-				</div>
-
-				{
-					uploadProgress !== null && (
-						<ProgressBar animated variant="success" now={uploadProgress} className="mb-3" />
-					)
-				}
-
-				{
-					alertMsg && (<Alert variant={alertMsg.type} className="my-3">{alertMsg.msg}</Alert>)
-				}
-
-				<div>
-					<Button variant="primary" type="submit">Upload</Button>
-					<Button variant="secondary" type="reset">Clear</Button>
-				</div>
-			</Form>
 		</Container>
 	);
 }
