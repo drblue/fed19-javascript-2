34c50c01b5c81fc39312f51d14b75520cafdfe9d
Johan Nordström <johan@digitalvillage.se>
2020-12-07 10:32:45 +0100

    Moved Navigation to its own component.

--- 07-snapgram/src/App.js
+++ 07-snapgram/src/App.js
@@ -1,38 +1,12 @@
-/* eslint-disable jsx-a11y/img-redundant-alt */
 import React from 'react'
-import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap'
-import logo from './assets/images/logo.svg'
+import { Container } from 'react-bootstrap'
+import Navigation from './components/Navigation'
 import './assets/scss/app.scss'
 
 const App = () => {
 	return (
 		<>
-			<Navbar bg="dark" variant="dark">
-				<Container>
-					<Navbar.Brand href="/">
-						<img
-							alt="A photo album"
-							src={logo}
-							width="30"
-							height="30"
-							className="d-inline-block align-top"
-						/>{' '}
-						Snapgram
-					</Navbar.Brand>
-
-					<Navbar.Toggle aria-controls="basic-navbar-nav" />
-					<Navbar.Collapse id="basic-navbar-nav">
-						<Nav className="ml-auto">
-							<Nav.Link href="/albums">Albums</Nav.Link>
-							<NavDropdown title="Username" id="basic-nav-dropdown">
-								<NavDropdown.Item href="/my-profile">My profile</NavDropdown.Item>
-								<NavDropdown.Divider />
-								<NavDropdown.Item href="/logout">Log Out</NavDropdown.Item>
-							</NavDropdown>
-						</Nav>
-					</Navbar.Collapse>
-				</Container>
-			</Navbar>
+			<Navigation />
 
 			<Container>
 				Here be content.
