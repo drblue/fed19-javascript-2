7723fc0a59a820b3a276707bd745c68f4d2968e3
Johan Nordström <johan@digitalvillage.se>
2020-12-09 14:03:08 +0100

    Added retrieving of route-parameter `albumId`.

--- 07-snapgram/src/components/albums/Album.js
+++ 07-snapgram/src/components/albums/Album.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 import AlbumImages from './AlbumImages'
 
 const Album = () => {
 	// query firestore for images with this albumId set as their album
+	const { albumId } = useParams()
 
 	return (
 		<div>
-			This will show all images in a specific album.
+			This will show all images in album with id <code>{albumId}</code>.
 
 			<AlbumImages images={[]} />
 		</div>
