91533b484efc33adb611623b55cd1abf58b9eb00
Johan Nordström <johan@digitalvillage.se>
2020-12-09 15:28:23 +0100

    WIP of showing a albums images.

--- 07-snapgram/src/components/albums/Album.js
+++ 07-snapgram/src/components/albums/Album.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import AlbumImages from './AlbumImages'
+import useAlbumImages from '../../hooks/useAlbumImages'
 
 const Album = () => {
 	// query firestore for images with this albumId set as their album
 	const { albumId } = useParams()
+	const { images } = useAlbumImages(albumId)
 
 	return (
 		<div>
 			This will show all images in album with id <code>{albumId}</code>.
 
-			<AlbumImages images={[]} />
+			<AlbumImages images={images} />
 		</div>
 	)
 }
--- 07-snapgram/src/components/albums/AlbumImages.js
+++ 07-snapgram/src/components/albums/AlbumImages.js
@@ -3,7 +3,15 @@ import React from 'react'
 const AlbumImages = ({ images }) => {
 	return (
 		<div>
-			This will show a grid of all images in this album.
+			This will show a grid of {images.length} images for this album.
+
+			<ul>
+				{images.length !== 0 && (
+					images.map(image => (
+						<li key={image.id}>{image.name}</li>
+					))
+				)}
+			</ul>
 		</div>
 	)
 }
