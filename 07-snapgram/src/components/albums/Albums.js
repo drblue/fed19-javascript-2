5f8960f0448e2dd2e8a9ed675b9966e926f33b16
Johan Nordström <johan@digitalvillage.se>
2020-12-09 15:28:07 +0100

    Get all available albums.

--- 07-snapgram/src/components/albums/Albums.js
+++ 07-snapgram/src/components/albums/Albums.js
@@ -1,20 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Row, Col, Card } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import { BounceLoader } from 'react-spinners'
 import { useAuth } from '../../contexts/AuthContext'
+import { db } from '../../firebase'
 
 const Albums = () => {
 	const { currentUser } = useAuth()
+	const [albums, setAlbums] = useState([])
+	const [loading, setLoading] = useState(true)
 
-	// query firestore for all available albums
+	useEffect(() => {
+		// register a snapshot-listener on firestore for all available albums
+		const unsubscribe = db.collection('albums').onSnapshot(snapshot => {
+			setLoading(true)
+			const snapshotAlbums = []
+
+			snapshot.forEach(doc => {
+				snapshotAlbums.push({
+					id: doc.id,
+					...doc.data(),
+				})
+			})
+
+			setAlbums(snapshotAlbums)
+			setLoading(false)
+		})
+
+		return unsubscribe
+	}, [])
 
 	return (
 		<>
-			<div>
-				This will show all albums.
-			</div>
+			<h2 className="mb-3">All Albums</h2>
+
+			{loading && (<BounceLoader color={"#888"} size={20} />)}
+
+			{!loading && (
+				<Row>
+					{albums.map(album => (
+						<Col sm={6} md={4} lg={3} key={album.id}>
+							<Card className="mb-3">
+								<Card.Body>
+									<Card.Title>
+										<Link to={`/albums/${album.id}`}>{album.title}</Link>
+									</Card.Title>
+								</Card.Body>
+							</Card>
+						</Col>
+					))}
+				</Row>
+			)}
+
 			{currentUser && (
-				<div>
-					<Link to="/albums/create">Create a new Album</Link>
+				<div className="mt-3">
+					<Link to="/albums/create" className="btn btn-primary">Create a new Album</Link>
 				</div>
 			)}
 		</>
