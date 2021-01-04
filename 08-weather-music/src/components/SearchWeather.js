7460d66e6384d9bde483d9a7189c4efbbb0e6942
Johan Nordström <johan@digitalvillage.se>
2021-01-04 12:02:11 +0100

    Enabled HTTPS and added a quick test for retrieving the user's position.

--- 08-weather-music/src/components/SearchWeather.js
+++ 08-weather-music/src/components/SearchWeather.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 import InputGroup from 'react-bootstrap/InputGroup'
@@ -6,6 +6,23 @@ import InputGroup from 'react-bootstrap/InputGroup'
 const SearchWeather = ({ onSearchWeather }) => {
 	const [userLocation, setUserLocation] = useState(false)
 	const [city, setCity] = useState("")
+
+	useEffect(() => {
+		const positionOptions = {
+			timeout: 10 * 1000, // 10 seconds
+			maximumAge: 2 * 60 * 60 * 1000, // 2 hours
+		}
+
+		const handleSuccess = position => {
+			console.log('Gots me a position:', position)
+		}
+
+		const handleError = error => {
+			console.error('Ooops, could not get a position:', error);
+		}
+
+		navigator.geolocation.getCurrentPosition(handleSuccess, handleError, positionOptions)
+	}, [])
 
 	const handleChange = (e) => {
 		setCity(e.target.value)
