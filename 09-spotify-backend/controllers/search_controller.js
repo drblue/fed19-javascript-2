5886b9615dfba95f871ab948ec101c164eb4840e
Johan Nordström <johan@digitalvillage.se>
2021-01-11 10:46:27 +0100

    Backend boilerplate for Spotify OAuth demo.

---
+++ 09-spotify-backend/controllers/search_controller.js
@@ -0,0 +1,12 @@
+/**
+ * Search Controller
+ */
+
+/**
+ * Search
+ *
+ * GET /
+ */
+module.exports = async (req, res) => {
+	res.status(405).send({ status: 'fail', message: 'Method Not Implemented.'});
+}
